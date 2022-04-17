2d174d5448ab48b0a706debab8aad34a4a5b8080
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-04-17 17:03:47 +0530

    database connection parameters saved to variables

--- BillService/src/main/java/util/DBConnection.java
+++ BillService/src/main/java/util/DBConnection.java
@@ -3,14 +3,20 @@ package util;
 import java.sql.*;
 
 public class DBConnection {
+	private static String driver = "com.mysql.jdbc.Driver";
+	private static String url = "jdbc:mysql://127.0.0.1:3306/";
+	private static String database = "ElectroGrid";
+	private static String username = "root";
+	private static String password = "";
+
     // common method to connect to DB
 	public static Connection connect() {
 		Connection connection = null;
 		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(driver);
 			//DB connection details
-			connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ElectroGrid","root","");
+			connection = DriverManager.getConnection(url + database, username, password);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
