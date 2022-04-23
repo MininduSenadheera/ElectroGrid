bf37018a40977b3050b21d87ca995e648249aa1a
sajiniPW <it20418274@my.sliit.lk>
2022-04-23 17:31:42 +0530

    Create payment bean

---
+++ PaymentService/src/main/java/bean/PaymentBean.java
@@ -0,0 +1,55 @@
+package bean;
+
+import java.sql.Timestamp;
+
+public class PaymentBean {
+	
+	private int paymentID;
+	private int customerID;
+	private int billID;
+	private Timestamp paymentDateTime;
+	private float amount;
+    private String type;
+    
+    
+    public int getPaymentID() {
+		return paymentID;
+	}
+	public void setPaymentID(int paymentID) {
+		this.paymentID = paymentID;
+	}
+	public int getCustomerID() {
+		return customerID;
+	}
+	
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	
+	public int getBillID() {
+		return billID;
+	}
+	public void setBillID(int billID) {
+		this.billID = billID;
+	}
+	
+	public Timestamp getPaymentDateTime() {
+		return paymentDateTime;
+	}
+	public void setPaymentDateTime(Timestamp paymentDateTime) {
+		this.paymentDateTime = paymentDateTime;
+	}
+   
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
