5a4f0c308535c11502d21f631380f3bfb707debd
varnavi430 <71841456+varnavi430@users.noreply.github.com>
2022-04-24 12:44:52 +0530

    bean_class

--- CustomerService/src/main/java/bean/CustomerBean.java
+++ CustomerService/src/main/java/bean/CustomerBean.java
@@ -4,5 +4,11 @@ package bean;
 import com.google.gson.*;
 
 public class CustomerBean {
-	//aaaaaaaaaaaaaaa
+	private int  customerId;
+	private String firstName;
+	private String lastName;
+	private String nic;
+	private int phoneNumber;
+	private String email;
+	private String address;
 }
