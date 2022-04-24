aa713b17e51bf2c2b154934227a037697d52995a
varnavi430 <71841456+varnavi430@users.noreply.github.com>
2022-04-24 12:51:48 +0530

    addingNewAttributesViaBeanClass

--- CustomerService/src/main/java/bean/CustomerBean.java
+++ CustomerService/src/main/java/bean/CustomerBean.java
@@ -23,6 +23,24 @@ public class CustomerBean {
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getNic() {
+		return nic;
+	}
+	public void setNic(String nic) {
+		this.nic = nic;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 	public String getEmail() {
 		return email;
 	}
@@ -35,4 +53,5 @@ public class CustomerBean {
 	public void setAddress(String address) {
 		this.address = address;
 	}
+	
 }
