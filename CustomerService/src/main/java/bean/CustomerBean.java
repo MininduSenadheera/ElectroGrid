d784249a4d311e4e48caa37ee6bbdc286ce8374e
varnavi430 <71841456+varnavi430@users.noreply.github.com>
2022-04-24 12:45:25 +0530

    geeter_setters

--- CustomerService/src/main/java/bean/CustomerBean.java
+++ CustomerService/src/main/java/bean/CustomerBean.java
@@ -11,4 +11,28 @@ public class CustomerBean {
 	private int phoneNumber;
 	private String email;
 	private String address;
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 }
