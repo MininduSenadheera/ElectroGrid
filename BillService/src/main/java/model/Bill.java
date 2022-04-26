2c1adf725abbd4ae6b95b5265ce1e3c4f2fc71c7
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-04-26 11:53:35 +0530

    added sql exception for delete method

--- BillService/src/main/java/model/Bill.java
+++ BillService/src/main/java/model/Bill.java
@@ -378,7 +378,10 @@ public class Bill {
             } else {
                 return Response.status(Status.NOT_FOUND).entity("No bills found with the corresponding ID").build();
             }
-            
+
+        } catch (SQLException se) {
+            System.err.println(se.getMessage());
+            return Response.status(Status.NOT_MODIFIED).entity(se.getMessage()).build(); 
         } catch (Exception e) {
             System.err.println(e.getMessage());
             return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
